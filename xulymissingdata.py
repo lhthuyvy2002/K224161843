770d8a4421b4e8412d91196b9e8672e90dacfe17
Thúy Vy <120909578+lhthuyvy2002@users.noreply.github.com>
2025-01-04 03:51:27 +0700

    Add files via upload

---
+++ xulymissingdata.py
@@ -0,0 +1,30 @@
+# Filtering out missing data
+from numpy import nan as NA
+import pandas as pd
+
+data = pd.DataFrame([[1., 6.5, 3.],
+                     [1., NA, NA],
+                     [NA, NA, NA],
+                     [NA, 6.5, 3.]])
+print(data)
+print("-"*10)
+cleaned = data.dropna()
+print(cleaned)
+cleaned2=data.dropna(how='all')
+print(cleaned2)
+
+# Filling in missing data
+from numpy import nan as NA
+import pandas as pd
+
+data = pd.DataFrame([[1., 6.5, 3.],
+                     [1., NA, NA],
+                     [NA, NA, NA],
+                     [NA, 6.5, 3.]])
+print(data)
+print("-"*10)
+cleaned=data.fillna(data.mean())
+print(cleaned)
+
+
+
